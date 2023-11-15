2601511fb7a396d1cbd076dd5ada1ffbc06580f3
Roberto Arcomano <info@robertoarcomano.com>
2023-11-15 18:48:04 +0100

    Added bernoulli_naive_bayes.py

---
+++ SciPy/bernoulli_naive_bayes.py
@@ -0,0 +1,32 @@
+import numpy as np
+from sklearn.naive_bayes import BernoulliNB
+
+
+rng = np.random.RandomState()
+X = rng.randint(5, size=(6, 2))
+T = rng.randint(5, size=(1, 2))
+print(X)
+print()
+print(T)
+Y = np.array([1, 0, 0, 1, 1, 0])
+clf = BernoulliNB(force_alpha=False)
+clf.fit(X, Y)
+
+total = []
+for i in range(len(X)):
+    my_sum = np.sum(X[i:i + 1] == T[0:1])
+    print("count for record", i, ":", np.sum(X[i:i+1] == T[0:1]), "(", Y[i], ")")
+    total.append([my_sum, i])
+
+
+def using_first(item):
+    return item[0]
+
+
+print(total)
+total = sorted(total,key=using_first)
+print(total)
+print("index:", total[-1][1])
+expected = Y[total[-1][1]]
+print("Expected:", expected)
+print("Got:", clf.predict(T[0:1]))
