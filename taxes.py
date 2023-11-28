1b9483da34b146eebe3852c100f4245afa16a3e9
Roberto Arcomano <info@robertoarcomano.com>
2023-11-28 01:23:29 +0100

    Refactored taxes.py.

--- taxes.py
+++ taxes.py
@@ -5,6 +5,8 @@ from matplotlib import style
 
 class Tax:
     def __init__(self, tax_data):
+        self.name = tax_data["name"]
+        self.color = tax_data["color"]
         self.taxes = np.array(tax_data["rates"])
         self.currency_factor = tax_data["currency_factor"]
         self.gross_incomes = np.array(tax_data["incomes"]) * self.currency_factor
@@ -19,6 +21,12 @@ class Tax:
             prev_gross = self.gross_incomes[i - 1] if i > 0 else 0
             prev_net = self.net_incomes[i - 1] if i > 0 else 0
             self.net_incomes.append(self.tax2rate(self.taxes[i]) * (self.gross_incomes[i] - prev_gross) + prev_net)
+
+    def get_name(self):
+        return self.name
+
+    def get_color(self):
+        return self.color
 
     @staticmethod
     def tax2rate(tax_item, tax_part=False):
@@ -38,16 +46,27 @@ class Tax:
         return np.array([self.get_net_income(gross_income) for gross_income in gross_incomes])
 
 
-nz_tax_data = {"rates": [10.5, 17.5, 30, 33, 39], "incomes": [14000, 48000, 70000, 180000], "currency_factor": 0.56}
-malta_tax_data = {"rates": [0, 15, 25, 35], "incomes": [9100, 14500, 60000], "currency_factor": 1,
+nz_tax_data = {"name": "NZ", "color": "red", "rates": [10.5, 17.5, 30, 33, 39],
+               "incomes": [14000, 48000, 70000, 180000], "currency_factor": 0.56}
+malta_tax_data = {"name": "Malta", "color": "orange", "rates": [0, 15, 25, 35], "incomes": [9100, 14500, 60000],
+                  "currency_factor": 1,
                   "capping": {"tax": 10, "yearly_cap": 2500}}
-italy_tax_data = {"rates": [23, 25, 35, 43], "incomes": [15000, 28000, 50000], "currency_factor": 1, "local_tax": 0.5}
-ireland_tax_data = {"rates": [20, 40], "incomes": [40000], "currency_factor": 1}
+italy_tax_data = {"name": "Italy", "color": "cyan", "rates": [23, 25, 35, 43], "incomes": [15000, 28000, 50000],
+                  "currency_factor": 1, "local_tax": 0.5}
+ireland_tax_data = {"name": "Ireland", "color": "yellow", "rates": [20, 40], "incomes": [40000], "currency_factor": 1}
+uk_tax_data = {"name": "UK", "color": "blue", "rates": [0, 20, 40, 45], "incomes": [12570, 50270, 125140],
+               "currency_factor": 1.15}
+us_tax_data = {"name": "US", "color": "pink", "rates": [10, 12, 22, 24, 32, 35, 37],
+               "incomes": [11000, 44725, 95375, 182100, 231250, 578125],
+               "currency_factor": 0.91}
 
 tax_nz = Tax(nz_tax_data)
 tax_malta = Tax(malta_tax_data)
 tax_italy = Tax(italy_tax_data)
 tax_ireland = Tax(ireland_tax_data)
+tax_uk = Tax(uk_tax_data)
+tax_us = Tax(us_tax_data)
+taxes = np.array([tax_nz, tax_malta, tax_italy, tax_ireland, tax_uk, tax_us])
 
 MIN = 1
 MAX = 200000
@@ -55,42 +74,19 @@ N = 100
 style.use("bmh")
 x = np.array(np.linspace(MIN, MAX, N))
 
-y_nz = tax_nz.get_net_incomes(x)
-y_malta = tax_malta.get_net_incomes(x)
-y_italy = tax_italy.get_net_incomes(x)
-y_ireland = tax_ireland.get_net_incomes(x)
-
-plot.subplot(221)
-plot.plot(x, y_nz, label="NZ Net Income", color="red")
-plot.plot(x, y_malta, label="Malta Net Income", color="green")
-plot.plot(x, y_italy, label="Italy Net Income", color="cyan")
-plot.plot(x, y_ireland, label="Ireland Net Income", color="brown")
-plot.xlabel("Gross Income")
-plot.ylabel("Net Income")
-plot.figlegend()
-
-plot.subplot(222)
-plot.plot(x, 100 * y_nz / x, label="NZ Net Income", color="red")
-plot.plot(x, 100 * y_malta / x, label="Malta Net Income", color="green")
-plot.plot(x, 100 * y_italy / x, label="Italy Net Income", color="cyan")
-plot.plot(x, 100 * y_ireland / x, label="Ireland Net Income", color="brown")
-plot.xlabel("Gross Income")
-plot.ylabel("% Net/Gross Income")
-
-plot.subplot(223)
-plot.plot(x, np.zeros(len(x)), label="Italy Net Income", color="cyan")
-plot.plot(x, y_malta-y_italy, label="Malta Net Income", color="green")
-plot.plot(x, y_ireland-y_italy, label="Ireland Net Income", color="brown")
-plot.plot(x, y_nz - y_italy, label="NZ Net Income", color="red")
-plot.xlabel("Gross Income")
-plot.ylabel("Net Income compared to Italy")
-
-plot.subplot(224)
-plot.plot(x, 100*np.ones(len(x)), label="Italy Net Income", color="cyan")
-plot.plot(x, 100*y_malta/y_italy, label="Malta Net Income", color="green")
-plot.plot(x, 100*y_ireland/y_italy, label="Ireland Net Income", color="brown")
-plot.plot(x, 100*y_nz/y_italy, label="NZ Net Income", color="red")
-plot.xlabel("Gross Income")
-plot.ylabel("% Net Income compared to Italy")
+y = np.array([
+    [lambda t: t.get_net_incomes(x), "Gross Income", "Net Income"],
+    [lambda t: 100 * t.get_net_incomes(x) / x, "Gross Income", "% Net/Gross Income"],
+    [lambda t: t.get_net_incomes(x) - tax_italy.get_net_incomes(x), "Gross Income", "Net Income compared to Italy"],
+    [lambda t: 100 * t.get_net_incomes(x) / tax_italy.get_net_incomes(x), "Gross Income",
+     "% Net Income compared to Italy"],
+])
+for j in range(len(y)):
+    plot.subplot(221 + j)
+    for tax in taxes:
+        plot.plot(x, y[j][0](tax), label=tax.get_name(), color=tax.get_color())
+        plot.xlabel(y[j][1])
+        plot.ylabel(y[j][2])
+    plot.figlegend() if not j else ""
 
 plot.show()
