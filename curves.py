0c53b34111c825a63849714ce516510f9c9adaf8
Roberto Arcomano <info@robertoarcomano.com>
2023-11-30 00:29:16 +0100

    put everything in a for loop.

--- curves.py
+++ curves.py
@@ -1,15 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plot
 
-x = np.linspace(0, 5, 100)
-y1 = np.exp(x)
-y2 = x
-y3 = x ** 2
-y4 = x ** 3
-plot.plot(x, y1, label="exp")
-plot.plot(x, y2, label="linear")
-plot.plot(x, y3, label="quadratric")
-plot.plot(x, y4, label="cubic")
+x = np.linspace(-5, 5, 1000)
+fns = np.array([
+    ["exp", lambda t: np.exp(t)],
+    ["log", lambda t: np.log(t)],
+    ["linear", lambda t: t],
+    ["quadratic", lambda t: t ** 2],
+    ["cubic", lambda t: t ** 3],
+    ["inverse", lambda t: 1/t],
+])
+for fn in fns:
+    plot.plot(x, fn[1](x), label=fn[0])
 plot.xlabel("x")
 plot.figlegend()
 plot.show()
