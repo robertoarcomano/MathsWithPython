0c8f86397806c20d9a75d00fba6bf5f57232558d
Roberto Arcomano <info@robertoarcomano.com>
2023-11-30 00:24:42 +0100

    Improved taxes.py with enumerate.

--- taxes.py
+++ taxes.py
@@ -46,52 +46,58 @@ class Tax:
     def get_net_incomes(self, gross_incomes):
         return np.vectorize(self.get_net_income)(gross_incomes)
 
+    def get_net_incomes_perc(self, gross_incomes):
+        return 100 * self.get_net_incomes(gross_incomes) / gross_incomes
 
-nz_tax_data = {"name": "NZ", "color": "red", "rates": [10.5, 17.5, 30, 33, 39],
-               "incomes": [14000, 48000, 70000, 180000], "currency_factor": 0.56}
-malta_tax_data = {"name": "Malta", "color": "orange", "rates": [0, 15, 25, 35], "incomes": [9100, 14500, 60000],
-                  "currency_factor": 1,
-                  "capping": {"tax": 10, "yearly_cap": 2500}}
-italy_tax_data = {"name": "Italy", "color": "cyan", "rates": [23, 25, 35, 43], "incomes": [15000, 28000, 50000],
-                  "currency_factor": 1, "local_tax": 0.5}
-ireland_tax_data = {"name": "Ireland", "color": "yellow", "rates": [20, 40], "incomes": [40000], "currency_factor": 1}
-uk_tax_data = {"name": "UK", "color": "blue", "rates": [0, 20, 40, 45], "incomes": [12570, 50270, 125140],
-               "currency_factor": 1.15}
-us_tax_data = {"name": "US", "color": "pink", "rates": [10, 12, 22, 24, 32, 35, 37],
-               "incomes": [11000, 44725, 95375, 182100, 231250, 578125],
-               "currency_factor": 0.91}
+    def get_net_incomes_reference(self, gross_incomes, reference_tax):
+        return self.get_net_incomes(gross_incomes) - reference_tax.get_net_incomes(gross_incomes)
 
-tax_nz = Tax(nz_tax_data)
-tax_malta = Tax(malta_tax_data)
-tax_italy = Tax(italy_tax_data)
-tax_ireland = Tax(ireland_tax_data)
-tax_uk = Tax(uk_tax_data)
-tax_us = Tax(us_tax_data)
-taxes = np.array([tax_nz, tax_malta, tax_italy, tax_ireland, tax_uk, tax_us])
+    def get_net_incomes_reference_perc(self, gross_incomes, reference_tax):
+        return 100 * self.get_net_incomes(gross_incomes) / reference_tax.get_net_incomes(gross_incomes)
+
+
+tax_data = [
+    {"name": "NZ", "color": "red", "rates": [10.5, 17.5, 30, 33, 39],
+     "incomes": [14000, 48000, 70000, 180000], "currency_factor": 0.56},
+    {"name": "Malta", "color": "orange", "rates": [0, 15, 25, 35], "incomes": [9100, 14500, 60000],
+     "currency_factor": 1,
+     "capping": {"tax": 10, "yearly_cap": 2500}},
+    {"name": "Italy", "color": "cyan", "rates": [23, 25, 35, 43], "incomes": [15000, 28000, 50000],
+     "currency_factor": 1, "local_tax": 0.5},
+    {"name": "Ireland", "color": "yellow", "rates": [20, 40], "incomes": [40000], "currency_factor": 1},
+    {"name": "UK", "color": "blue", "rates": [0, 20, 40, 45], "incomes": [12570, 50270, 125140],
+     "currency_factor": 1.15},
+    {"name": "US", "color": "pink", "rates": [10, 12, 22, 24, 32, 35, 37],
+     "incomes": [11000, 44725, 95375, 182100, 231250, 578125],
+     "currency_factor": 0.91}
+]
+taxes = [Tax(tax_i) for tax_i in tax_data]
+tax_reference = [tax_i for tax_i in taxes if tax_i.get_name() == "Italy"][0]
 
 MIN = 1
 MAX = 200000
 N = 100
-XLABEL = "Gross Income [Euro]"
-FONTSIZE = 4
-BIG_FONTSIZE = 5
-LINEWIDTH = 0.7
+X_LABEL = "Gross Income [Euro]"
+FONT_SIZE = 4
+BIG_FONT_SIZE = 5
+LINE_WIDTH = 0.7
 style.use("bmh")
 x = np.array(np.linspace(MIN, MAX, N))
-matplotlib.rc('xtick', labelsize=FONTSIZE)
-matplotlib.rc('ytick', labelsize=FONTSIZE)
-plot.rcParams.update({'font.size': BIG_FONTSIZE})
-y = np.array([
-    [lambda t: t.get_net_incomes(x), XLABEL, "Net Income [Euro]"],
-    [lambda t: 100 * t.get_net_incomes(x) / x, XLABEL, "% Net/Gross Income [Euro]"],
-    [lambda t: t.get_net_incomes(x) - tax_italy.get_net_incomes(x), XLABEL, "Net Income compared to Italy [Euro]"],
-    [lambda t: 100 * t.get_net_incomes(x) / tax_italy.get_net_incomes(x), XLABEL,
+matplotlib.rc('xtick', labelsize=FONT_SIZE)
+matplotlib.rc('ytick', labelsize=FONT_SIZE)
+plot.rcParams.update({'font.size': BIG_FONT_SIZE})
+
+graphs = np.array([
+    [lambda t: t.get_net_incomes(x), X_LABEL, "Net Income [Euro]"],
+    [lambda t: t.get_net_incomes_perc(x), X_LABEL, "% Net/Gross Income [Euro]"],
+    [lambda t: t.get_net_incomes_reference(x, tax_reference), X_LABEL, "Net Income compared to Italy [Euro]"],
+    [lambda t: t.get_net_incomes_reference_perc(x, tax_reference), X_LABEL,
      "% Net Income compared to Italy"],
 ])
-for graph_i, (f, x_label, y_label) in enumerate(y):
+for graph_i, (f, x_label, y_label) in enumerate(graphs):
     plot.subplot(221 + graph_i)
     for tax in taxes:
-        plot.plot(x, f(tax), label=tax.get_name(), color=tax.get_color(), linewidth=LINEWIDTH)
+        plot.plot(x, f(tax), label=tax.get_name(), color=tax.get_color(), linewidth=LINE_WIDTH)
         plot.xlabel(x_label)
         plot.ylabel(y_label)
     plot.figlegend() if not graph_i else ""
